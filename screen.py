16ef85feaa4a96c909abe8324933a15a1448ad5b
Great-Szu <mateusz.chol.net@outlook.com>
2025-06-28 23:37:23 +0200

    Day 3 building first GUI, populiating fields with text, scrolls, spinners

---
+++ screen.py
@@ -0,0 +1,105 @@
+import os
+import tkinter
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Grid Demo")
+mainWindow.geometry('640x480-8-200')
+
+label = tkinter.Label(mainWindow, text='Tkinter Grid Demo')
+label.grid(row=0, column=0, columnspan=3)
+
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.columnconfigure(2, weight=3)
+mainWindow.columnconfigure(3, weight=3)
+mainWindow.columnconfigure(4, weight=3)
+
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=10)
+mainWindow.rowconfigure(2, weight=1)
+mainWindow.rowconfigure(3, weight=3)
+mainWindow.rowconfigure(4, weight=3)
+
+# White field on the most left
+fileList = tkinter.Listbox(mainWindow)
+fileList.grid(row=1, column=0, sticky='nsew', rowspan=2)
+fileList.config(border=2, relief='sunken')
+
+# Populating data to the field on the left
+for foldername in os.listdir('C:/Users/panmi'):
+    fileList.insert(tkinter.END, foldername)
+
+# Adding scrollbar. Command lets the scroll bar control the fieldList.
+listScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=fileList.yview)
+listScroll.grid(row=1, column=1, sticky='nsw', rowspan=2)
+fileList['yscrollcommand'] = listScroll.set # This line updates the widget so If user uses scroll then in also activates
+
+# Frame for the radio buttons
+optionFrame = tkinter.LabelFrame(mainWindow, text='File Details')
+optionFrame.grid(row=1, column=2, sticky='ne')
+optionFrame.config(border=2,relief='groove')
+
+
+rbValue = tkinter.IntVar() # Setting to one choice at a time
+rbValue.set(1) # Default 'value' set
+
+# Creating the options, 'value' set the triggered value when clicked
+radio1 = tkinter.Radiobutton(optionFrame, text="Filename", value=1, variable=rbValue)
+radio2 = tkinter.Radiobutton(optionFrame, text="Path", value=2, variable=rbValue)
+radio3 = tkinter.Radiobutton(optionFrame, text="Timestamp", value=3, variable=rbValue)
+radio1.grid(row=0, column=0, sticky='w')
+radio2.grid(row=1, column=0, sticky='w')
+radio3.grid(row=2, column=0, sticky='w')
+
+# Adding the text field
+resultLabel = tkinter.Label(mainWindow, text='Result')
+resultLabel.grid(row=2, column=2, sticky='nw')
+result = tkinter.Entry(mainWindow) # method for text field
+result.grid(row=2, column=2, sticky='sw')
+
+
+# Frame for the time spinners
+timeFrame = tkinter.LabelFrame(mainWindow, text='Time')
+timeFrame.grid(row=3, column=0, sticky='new')
+timeFrame.config(relief='flat')
+# Time spinners
+hourSpinner = tkinter.Spinbox(timeFrame, width=2, values=tuple(range(0, 24)))
+minuteSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+secondSpinner = tkinter.Spinbox(timeFrame, width=2, from_=0, to=59)
+hourSpinner.grid(row=0, column=0)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=1)
+minuteSpinner.grid(row=0, column=2)
+tkinter.Label(timeFrame, text=':').grid(row=0, column=3)
+secondSpinner.grid(row=0, column=4)
+timeFrame['padx'] = 36
+
+# Frame for the date spinners
+dateFrame = tkinter.Frame(mainWindow)
+dateFrame.grid(row=4, column=0, sticky='new')
+# Date labels
+dayLabel = tkinter.Label(dateFrame, text='Day')
+monthLabel = tkinter.Label(dateFrame, text='Month')
+yearLabel = tkinter.Label(dateFrame, text='Year')
+dayLabel.grid(row=0, column=0, sticky='w')
+monthLabel.grid(row=0, column=1, sticky='w')
+yearLabel.grid(row=0, column=2, sticky='w')
+
+#Date spinners
+daySpinner = tkinter.Spinbox(dateFrame, width=5, from_=0, to=31)
+monthSpinner = tkinter.Spinbox(dateFrame, width=5, values=('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+yearSpinner = tkinter.Spinbox(dateFrame, width=5, from_=2000, to=2099)
+daySpinner.grid(row=1, column=0)
+monthSpinner.grid(row=1, column=1)
+yearSpinner.grid(row=1, column=2)
+
+mainWindow.mainloop()
+print(rbValue.get())
+
+
+
+
+
+
+
+
